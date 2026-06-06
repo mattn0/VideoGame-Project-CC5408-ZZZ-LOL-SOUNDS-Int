6f11ef8323dfbb0df60828201770649b6095d686
Elmarsi0 <maximiliano.briceno@ug.uchile.cl>
2026-06-06 15:14:09 -0400

    Implementation of the store's functionality, among other things. (2.0)

--- ui/game_over.gd
+++ ui/game_over.gd
@@ -3,14 +3,14 @@ extends Control
 @onready var retry: Button = %Retry
 @onready var settings: Button = %Settings
 @onready var main_menu: Button = %MainMenu
-@onready var quit:Button = %Quit
+@onready var quit: Button = %Quit
 
 func _ready() -> void:
 	hide()
 	retry.pressed.connect(_on_retry_pressed)
 	settings.pressed.connect(_on_settings_pressed)
 	main_menu.pressed.connect(_on_main_menu_pressed)
-#	quit.pressed.connect(_on_quit_pressed)
+	quit.pressed.connect(_on_quit_pressed)
 
 func show_game_over() -> void:
 	print("MOSTRANDO GAME OVER")
@@ -18,8 +18,8 @@ func show_game_over() -> void:
 	visible = get_tree().paused
 	
 
-#func _on_quit_pressed() -> void:
-#	get_tree().quit()
+func _on_quit_pressed() -> void:
+	get_tree().quit()
 
 func _on_settings_pressed() -> void:
 	Debug.log("TODO")
